ee07e4e2c0833515232279b685c3664b531c6285
Aadya Sharma <138554910+Aadya242005@users.noreply.github.com>
2025-08-31 21:51:34 +0530

    Add files via upload

---
+++ lec21/Palindrome_part.java
@@ -0,0 +1,99 @@
+// package lec21;
+
+// public class Palindrome_part {
+//     public static void main(String[] args) {
+//         String ques = "nitin";
+//         Partitioning(ques, "");
+        
+//     }
+//     public static void Partitioning(String ques, String ans){
+//         if(ques.length()==0){
+//             System.out.println(ans);
+//             return;
+//         }
+//         for(int i=1; i<=ques.length(); i++){
+//             String s = ques.substring(0,i);
+//             Partitioning(ques.substring(i),ans + s+ "|");
+//         }
+//     }
+    
+// }
+
+
+// package lec21;
+
+// public class Palindrome_part {
+//     public static void main(String[] args) {
+//         String ques = "nitin";
+//         Partitioning(ques, "");
+        
+//     }
+//     public static void Partitioning(String ques, String ans){
+//         if(ques.length()==0){
+//             System.out.println(ans);
+//             return;
+//         }
+//         for(int i=1; i<=ques.length(); i++){
+//             String s = ques.substring(0,i);
+//             if(isPalindrome(s)){
+//                 Partitioning(ques.substring(i),ans + s+ "|");
+//             }
+//         }
+            
+//     }  
+//     public static boolean isPalindrome(String s){
+//         int i=0;
+//         int j= s.length()-1;
+//         while(i<j){
+//             if(s.charAt(i)!=s.charAt(j)){
+//                 return false;
+//             }
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }   
+// }
+
+
+
+package lec21;
+import java.util.*;
+public class Palindrome_part {
+    public static void main(String[] args) {
+        String ques = "nitin";
+        List<String> ll = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
+        Partitioning(ques, ll,ans);
+        System.out.println(ans);
+        
+    }
+    public static void Partitioning(String ques, List<String> ll, List<List<String>>ans){
+        if(ques.length()==0){
+            //System.out.println(ll);
+            ans.add(new ArrayList<>(ll));
+            return;
+        }
+        for(int i=1; i<=ques.length(); i++){
+            String s = ques.substring(0,i);
+            if(isPalindrome(s)){
+                ll.add(s);
+                Partitioning(ques.substring(i),ll,ans);
+                ll.remove(ll.size()-1);
+            }
+        }
+            
+    }  
+    public static boolean isPalindrome(String s){
+        int i=0;
+        int j= s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }   
+}
---
+++ lec22/Tower_of_Hanoi.java
@@ -0,0 +1,17 @@
+package lec22;
+
+public class Tower_of_Hanoi {
+    public static void main(String[] args){
+        int n=3;
+        TOH(n, "A","C","B");
+
+    }
+    public static void TOH(int n, String src, String hlp, String des){
+        if(n==0){
+            return;
+        }
+        TOH(n-1, src, des,hlp);
+        System.out.println("Move" + n + "th disk from"+ src+"to"+des);
+        TOH(n-1, hlp, src, des);
+    }   
+}
