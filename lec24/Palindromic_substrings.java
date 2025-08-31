7a55cd44a523ccf99e35b4f8511f5256fb94d05a
Aadya Sharma <138554910+Aadya242005@users.noreply.github.com>
2025-08-31 21:52:34 +0530

    Add files via upload

---
+++ lec24/Palindromic_substrings.java
@@ -0,0 +1,34 @@
+package lec24;
+
+public class Palindromic_substrings {
+    public static void main(String[] args) {
+        String s ="nitin";
+        System.out.println(CountPalindrome(s));
+
+        
+    }
+    public static int CountPalindrome(String s){
+        int odd =0; 
+        for(int axis =0;axis<s.length();axis++){
+            for(int orbit=0; axis-orbit>=0 && axis+orbit<s.length();orbit++){
+                if(s.charAt(axis-orbit)!=s.charAt(axis+orbit)){
+                break;
+                }
+                odd++;
+            }
+        }
+    
+        int even =0;
+        for(double axis =0.5;axis<s.length();axis++){
+            for(double orbit=0.5; axis-orbit>=0 && axis+orbit<s.length();orbit++){
+                if(s.charAt((int)(axis-orbit))!=s.charAt((int)(axis+orbit))){
+                break;
+                }
+                even++;
+            }
+        
+        }
+        return odd + even;
+    }
+}
+
