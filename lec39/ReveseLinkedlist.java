4d27ae74957e1690471e19379d041bf1898c0283
Aadya Sharma <138554910+Aadya242005@users.noreply.github.com>
2025-11-01 01:38:38 +0530

    Add files via upload

---
+++ lec39/ReveseLinkedlist.java
@@ -0,0 +1,39 @@
+package lec39;
+
+public class ReveseLinkedlist {
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(){
+        }
+        ListNode(int val){
+            this.val = val;
+        }
+        ListNode(int val, ListNode next){
+            this.val = val;
+            this.next = next;
+        }
+    }
+    class Solution{
+        public ListNode reverseList(ListNode head){
+           // return reverse(head, null);
+           ListNode curr = head;
+           ListNode prev = null;
+           while(curr!=null){
+            ListNode ahead = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = ahead;
+           }
+           return curr;
+        }
+        public ListNode reverse(ListNode curr, ListNode prev){
+            if(curr == null){
+                return prev;
+            }
+            ListNode temp = reverse(curr.next, curr);
+            curr.next= prev;
+            return temp;
+        }
+    }
+}
